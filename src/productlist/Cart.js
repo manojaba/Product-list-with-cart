a08240b89e518df5ab9ad9711ff9592f1e505a32
Manoj Baskar <147849968+manojaba@users.noreply.github.com>
2024-11-26 17:19:06 +0530

    states updated

--- src/productlist/Cart.js
+++ src/productlist/Cart.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React from 'react';
 
-function Cart() {
+function Cart({ cartCount, cartProduct, setCartProduct }) {
+
+
+
+
+
+
+
+
+    function deleteData(name) {
+        const localdata = localStorage.getItem('product');
+        let list = localdata ? JSON.parse(localdata) : [];
+        let remList = list.filter((each) => each.name !== name);
+        localStorage.setItem('product', JSON.stringify(remList));
+        setCartProduct(remList);
+
+    }
+
     return (
-        <div>Cart</div>
+        <div>
+            <h1>Your Cart({cartCount})</h1>
+            {
+                cartProduct.length > 0 ? (
+
+                    cartProduct.map((product, index) => {
+                        return (
+                            <div key={product.name} className='flex justify-between'>
+                                <div>
+                                    <p>{product.name}</p>
+                                    <p><span>{product.qty}x</span><span>@ ${product.price}</span><span>${product.qty * product.price}</span></p>
+                                </div>
+                                <button className=''><img className='border border-[#AD8A85] p-[2px] rounded-full' src='./assets/images/icon-remove-item.svg' onClick={(e) => deleteData(product.name)}></img></button>
+                            </div>
+                        )
+                    })
+
+                )
+                    : (
+                        <p> no data available</p>
+                    )
+            }
+
+        </div>
     )
 }
 
--- src/productlist/Home.js
+++ src/productlist/Home.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Cart from './Cart';
 import data from './data.json';
 import Product from './Product';
 
@@ -6,18 +7,71 @@ import Product from './Product';
 function Home() {
 
 
-    const product = data[0];
-    const { thumbnail, mobile, tablet, desktop } = product.image;
+
+
+    const [cartProduct, setCartProduct] = useState(() => {
+        const savedCart = localStorage.getItem('product');
+        return savedCart ? JSON.parse(savedCart) : [];
+    });
+
+    console.log(`cart product : ${cartProduct.length}`)
+
+    const [cartCount, setCartCount] = useState(() => {
+        const savedCartCount = localStorage.getItem('cart');
+        return savedCartCount ? parseInt(savedCartCount, 10) : 0;
+    });
+
+
+    useEffect(() => {
+        localStorage.setItem('product', JSON.stringify(cartProduct));
+        localStorage.setItem('cart', (cartProduct.length));
+        setCartCount(cartProduct.reduce((total, item) => total + item.qty, 0));
+    }, [cartProduct])
+
+
+
+
+
+    const addProduct = (thumbnail, name, category, price, qty) => {
+        setCartProduct((prev) => {
+            const existingProduct = prev.find((item) => item.name === name);
+            if (existingProduct) {
+                return prev.map((item) =>
+                    item.name === name ? { ...item, qty: item.qty + qty } : item
+                );
+            }
+            return [...prev, { thumbnail, name, category, price, qty }];
+        });
+    };
+
+    const decrementProduct = (name) => {
+        setCartProduct((prev) => {
+            const existingProduct = prev.find((item) => item.name === name);
+            if (existingProduct.qty > 1) {
+                return prev.map((item) =>
+                    item.name === name ? { ...item, qty: item.qty - 1 } : item
+                );
+            }
+            return prev.filter((item) => item.name !== name);
+        });
+    };
+
+
 
 
 
     return (
-        <div>
+        <div className='flex'>
 
-            <div className='w-4 h-4 bg-red-500'></div>
-            {
-                data.map((item) => <Product data={item} />)
-            }
+
+            <div>
+                <h1>{cartCount}</h1>
+                {
+                    data.map((item, index) => <Product key={index} data={item} addProduct={addProduct} decrementProduct={decrementProduct} />)
+                }
+            </div>
+
+            <Cart cartCount={cartCount} cartProduct={cartProduct} setCartProduct={setCartProduct} />
 
 
 
